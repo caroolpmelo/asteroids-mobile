8e3b6cf74d14adaa377c64a84aef838975f27e99
Carolina Melo <caroolpmelo@gmail.com>
2022-10-06 19:41:14 -0300

    :rocket: WIP: Ship initial movements
    Signed-off-by: Carolina Melo <caroolpmelo@gmail.com>

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rigidBody.gravityScale = 0;
+
+        if (rotationSpeed == 0)
+        {
+            rotationSpeed = 100f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // TODO: change input axis to touch
+        transform.Rotate(
+            0,
+            0,
+            -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+
+        rigidBody.AddForce(transform.up * Input.GetAxis("Vertical"));
     }
 }
